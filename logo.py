cbc6535e4646bc23a2d1176a61836914a1672dbb
Alex Clark <aclark@aclark.net>
2021-08-03 16:27:55 -0400

    Update

--- logo.py
+++ logo.py
@@ -5,11 +5,11 @@ import sys
 out = Image.new("RGB", (400, 400), (255, 255, 255))
 
 # get a font
-fnt = ImageFont.truetype("FreeMono.ttf", 320)
+fnt = ImageFont.truetype("FreeMono.ttf", 350)
 # get a drawing context
 d = ImageDraw.Draw(out)
 
 # draw multiline text
-d.multiline_text((10,10), "HS", font=fnt, fill=(0, 0, 0))
+d.multiline_text((10,10), "Hs", font=fnt, fill=(0, 0, 0))
 
 out.save("logo.png", "PNG")
