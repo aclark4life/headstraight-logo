c0a2ab79a2371bca79a1f6ed63da86423fd804e3
Alex Clark <aclark@aclark.net>
2021-08-03 17:01:23 -0400

    Update

--- logo.py
+++ logo.py
@@ -1,17 +1,24 @@
 # Via https://pillow.readthedocs.io/en/stable/reference/ImageDraw.html#
 
-from PIL import Image, ImageDraw, ImageFont
+import os
 import sys
 
+from PIL import Image, ImageDraw, ImageFont
+
 # create an image
-out = Image.new("RGB", (400, 400), (255, 255, 255))
 
-# get a font
-fnt = ImageFont.truetype("FreeMono.ttf", 300)
-# get a drawing context
-d = ImageDraw.Draw(out)
+files = [f for f in os.listdir(".") if f.endswith("ttf")]
 
-# draw multiline text
-d.multiline_text((30, 40), "Hs", font=fnt, fill=(0, 0, 0))
+for filename in files:
+    out = Image.new("RGB", (400, 400), (255, 255, 255))
+    # get a font
+    fnt = ImageFont.truetype(filename, 300)
+    print(filename)
+    # get a drawing context
+    d = ImageDraw.Draw(out)
 
-out.save("logo.png", "PNG")
+    # draw multiline text
+    d.multiline_text((30, 40), "Hs", font=fnt, fill=(0, 0, 0))
+
+    fontname = filename.split(".")[0]  # Remove file extension
+    out.save("logo_%s.png" % fontname, "PNG")
